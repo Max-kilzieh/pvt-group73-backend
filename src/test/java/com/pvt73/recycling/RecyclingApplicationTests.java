ed1b199b54e10ab5e866b029d1f40756079b8d7d
Max Kilzieh <mkilzieh@gmail.com>
2021-03-31 15:26:20 +0200

    Initial commit

---
+++ src/test/java/com/pvt73/recycling/RecyclingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvt73.recycling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyclingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
