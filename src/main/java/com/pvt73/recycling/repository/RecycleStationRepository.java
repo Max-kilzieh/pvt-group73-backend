c6c4accbe2bb805f3176c2cc7af1e9a17ad3b780
Max Kilzieh <mkilzieh@gmail.com>
2021-04-04 23:41:24 +0200

    Created RecycleStation model, RecycleStationRepository, and populated the database with recycling stations data.

--- src/main/java/com/pvt73/recycling/repository/RecycleStationRepository.java
+++ src/main/java/com/pvt73/recycling/repository/RecycleStationRepository.java
@@ -3,9 +3,7 @@ package com.pvt73.recycling.repository;
 import com.pvt73.recycling.model.RecycleStation;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
-public interface RecycleStationRepository extends CrudRepository<RecycleStation,Integer> {
+public interface RecycleStationRepository extends CrudRepository<RecycleStation, Integer> {
 
 
 }
