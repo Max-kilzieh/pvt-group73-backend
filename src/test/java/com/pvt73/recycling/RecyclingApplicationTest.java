6c402589d4f7d92cb26fa76a99e3b23b85e1736f
Max Kilzieh <mkilzieh@gmail.com>
2021-04-16 18:48:33 +0200

    Updated application context test.

--- src/test/java/com/pvt73/recycling/RecyclingApplicationTest.java
+++ src/test/java/com/pvt73/recycling/RecyclingApplicationTest.java
@@ -1,11 +1,20 @@
 package com.pvt73.recycling;
 
+import com.pvt73.recycling.controller.WasteBinController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class RecyclingApplicationTest {
+
+    @Autowired
+    private WasteBinController wasteBinController;
+
     @Test
     public void contextLoads() {
+        assertThat(wasteBinController).isNotNull();
     }
 }
