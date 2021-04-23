bea1c0ff12fd0a1fb62e2f61068d0d05dfdbde73
Max Kilzieh <mkilzieh@gmail.com>
2021-04-23 18:59:32 +0200

    test

--- src/main/java/com/pvt73/recycling/controller/ImageController.java
+++ src/main/java/com/pvt73/recycling/controller/ImageController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("t")
+@RequestMapping("/t")
 public class ImageController {
 
     private final ImageService imageService;
