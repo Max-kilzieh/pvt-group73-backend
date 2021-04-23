4dc7a650e68bca761e1561c084e3791e69efc719
Max Kilzieh <mkilzieh@gmail.com>
2021-04-23 16:48:04 +0200

    Specified post mapping

--- src/main/java/com/pvt73/recycling/controller/ImageController.java
+++ src/main/java/com/pvt73/recycling/controller/ImageController.java
@@ -7,10 +7,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -24,7 +21,7 @@ public class ImageController {
         this.imageService = imageService;
     }
 
-    @PostMapping("/upload")
+    @RequestMapping(value = "/upload",method = RequestMethod.POST)
     public Image uploadImage(@RequestParam("image") MultipartFile image) throws IOException {
 
         return imageService.saveImage(image);
