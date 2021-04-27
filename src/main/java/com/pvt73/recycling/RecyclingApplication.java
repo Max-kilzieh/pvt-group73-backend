b1a20a2143e9f0a6537bf2f4b8dfb8eeed3c6511
Max Kilzieh <mkilzieh@gmail.com>
2021-04-27 12:02:14 +0200

    Added logging to file pvt73.log

--- src/main/java/com/pvt73/recycling/RecyclingApplication.java
+++ src/main/java/com/pvt73/recycling/RecyclingApplication.java
@@ -1,13 +1,10 @@
 package com.pvt73.recycling;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RecyclingApplication {
-//     static final Logger LOGGER= LoggerFactory.getLogger(RecyclingApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(RecyclingApplication.class);
     }
