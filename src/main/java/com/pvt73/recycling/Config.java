f25c393a51a650c8e548a700ce7b445b0461295e
Max Kilzieh <mkilzieh@gmail.com>
2021-05-20 02:03:13 +0200

    Moved all configuration to Config.java
    Signed-off-by: Max Kilzieh <mkilzieh@gmail.com>

--- src/main/java/com/pvt73/recycling/Config.java
+++ src/main/java/com/pvt73/recycling/Config.java
@@ -3,8 +3,23 @@ package com.pvt73.recycling;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.google.maps.GeoApiContext;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "KeepItClean API",
+                description = "Group 73",
+                version = "0.6",
+                contact = @Contact(
+                        name = "Max Kilzieh",
+                        url = "https://github.com/Max-kilzieh",
+                        email = "mkilzieh@gmail.com"
+                )
+        ))
 
 @Configuration
 public class Config {
